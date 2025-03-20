22a46169dacfaad4a37e2763db27d14b4da1ce0f
Keyur <work.enma@gmail.com>
2025-03-21 09:46:03 +1100

    Leetcode - 167 (Two Sum II - Input Array Is Sorted) with Solution

---
+++ Leetcode/Medium/Leetcode - 167 (Two Sum II - Input Array Is Sorted)/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        low = 0
+        high = n-1
+        while low < high:
+            total = numbers[low] + numbers[high]
+            if total == target:
+                return [low+1, high+1]
+            elif total < target:
+                low = low + 1
+            else:
+                high = high - 1
